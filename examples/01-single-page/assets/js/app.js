cfab5b0020140e12e66a4189671e7a01200f811e
Julien M'Poy <julien.mpoy@gmail.com>
2019-11-05 22:29:21 +0100

    Add babel and production flag

--- examples/01-single-page/assets/js/app.js
+++ examples/01-single-page/assets/js/app.js
@@ -1,3 +1,5 @@
+import '../scss/styles.scss';
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Document is loaded');
 });
--- examples/01-single-page/webpack.config.js
+++ examples/01-single-page/webpack.config.js
@@ -4,14 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const outputDir = path.resolve(__dirname, 'public/build');
 
 module.exports = {
-  entry: {
-    app: './assets/js/app.js',
-    styles: './assets/scss/styles.scss',
-  },
+  mode: process.env.NODE_ENV,
+  entry: [
+    './assets/js/app.js',
+  ],
   output: {
     path: outputDir,
+    filename: 'js/[name].[contenthash].js'
   },
-  watch: true,
+  watch: process.env.NODE_ENV !== 'production',
   devServer: {
     contentBase: path.join(__dirname, 'assets'),
     watchContentBase: true,
@@ -26,6 +27,13 @@ module.exports = {
   ],
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
       {
         test: /\.s[ac]ss$/i,
         use: [
