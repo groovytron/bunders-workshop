253f764debb1a983e729ab5ddcab84295f236b50
Julien M'Poy <julien.mpoy@gmail.com>
2019-11-06 22:26:44 +0100

    Create webpack config for Symfony

---
+++ examples/03-symfony/assets/js/app.js
@@ -0,0 +1,13 @@
+import '../scss/styles.scss';
+import '../scss/other_styles.scss';
+import Square from './Square.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Document is loaded');
+  console.log('This is awesome');
+  console.log('This is a test');
+
+  const square = new Square(5);
+  console.log(square);
+  console.log(square.area());
+});
---
+++ examples/03-symfony/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+const outputDir = path.resolve(__dirname, 'public/build');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: [
+    './assets/js/app.js',
+  ],
+  output: {
+    path: outputDir,
+    filename: 'js/[name].[contenthash].js'
+  },
+  // watch: process.env.WEBPACK_MODE !== 'production',
+  plugins: [
+    new ManifestPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: 'style-loader', options: { injectType: 'linkTag' } },
+          {
+            loader: 'file-loader',
+            options: {
+                name: 'css/[name].[contenthash].css',
+            }
+          },
+          'extract-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  }
+};
